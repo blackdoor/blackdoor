80e3511e02aaac96ba91407857f2a4d056e78638
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2013-08-28 13:44:45 -0700

    cmon, merge

--- src/blackdoor/auth/AuthClient.java
+++ src/blackdoor/auth/AuthClient.java
@@ -20,7 +20,7 @@ import java.net.UnknownHostException;
 import blackdoor.auth.AuthRequest.CSHI;
 import blackdoor.auth.AuthRequest.Operation;
 import blackdoor.auth.User.UserRight;
-import blackdoor.util.Hash;
+import blackdoor.util.Crypto;
 
 /**
  * @author kAG0
@@ -204,7 +204,7 @@ public class AuthClient {
 		byte [] saltedHash = new byte[salt.length + passwordHash.length];
 		System.arraycopy(salt, 0, saltedHash, 0, salt.length);
 		System.arraycopy(passwordHash, 0, saltedHash, salt.length, passwordHash.length);
-		return Hash.getSHA1(saltedHash);
+		return Crypto.getSHA1(saltedHash);
 	}
 	
 	/**
@@ -259,7 +259,7 @@ public class AuthClient {
 	}
 	
 	private byte[] Hash(String string){	
-		return blackdoor.util.Hash.getSHA1(string.getBytes());
+		return blackdoor.util.Crypto.getSHA1(string.getBytes());
 	}
 		
 	private void openSocketOutput() throws Exception{
