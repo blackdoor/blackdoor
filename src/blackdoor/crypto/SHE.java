edba9fafe7b278806fcfea35a40bef007ef51344
Nathan Fischer <nfischer3@zagmail.gonzaga.edu>
2014-02-07 20:39:50 -0800

    Added an EncryptedOutputStream inner class for SHE

--- src/blackdoor/crypto/SHE.java
+++ src/blackdoor/crypto/SHE.java
@@ -3,6 +3,9 @@
  */
 package blackdoor.crypto;
 
+import java.io.FilterOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import java.io.Serializable;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
@@ -18,20 +21,44 @@ import blackdoor.util.Misc;
  */
 public class SHE {
 	
-	public static final int BLOCKSIZE = 32;
+	public int blockSize;// = 32;
 	private int blockNo;
 	private byte[] IV;
 	private byte[] key;
 	private boolean cfg;
-	private byte[] buffer = new byte[BLOCKSIZE];
+	private byte[] buffer = new byte[blockSize];
 	private int bufferIndex; //index at which to place next byte in buffer
 	private MessageDigest mD;
 	
-	//parameters for different algo and block sizes will go here
+	/**
+	 * Creates a Cipher object with specified algorithm.
+	 * @param algorithm the algorithm to use for this cipher.
+	 */
+	public SHE(String algorithm) throws NoSuchAlgorithmException{
+		blockNo = 0;
+		cfg = false;
+		mD = MessageDigest.getInstance(algorithm);
+		blockSize = mD.getDigestLength();
+	}
+	
+	/**
+	 * Creates a Cipher object.
+	 */
+	public SHE(Algorithm algorithm){
+		blockNo = 0;
+		cfg = false;
+		try {
+			mD = MessageDigest.getInstance(algorithm.getAlgorithm());
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		blockSize = mD.getDigestLength();
+	}
 	/**
 	 * Creates a Cipher object.
 	 */
 	public SHE(){
+		blockSize = 32;
 		blockNo = 0;
 		cfg = false;
 		try {
@@ -47,7 +74,7 @@ public class SHE {
 	 * @return A 256 bit IV that has been created for this cipher to use.
 	 */
 	public byte[] init(byte[] key){
-		byte[] iv = new byte[BLOCKSIZE];
+		byte[] iv = new byte[blockSize];
 		new SecureRandom().nextBytes(iv);
 		init(iv, key);
 		return iv;
@@ -59,25 +86,30 @@ public class SHE {
 	 * @param key A 256 bit key to encrypt with.
 	 */
 	public void init(byte[] IV, byte[] key){
-		if(IV.length != BLOCKSIZE || key.length != BLOCKSIZE)
-			throw new RuntimeException("key and IV need to be same as block size (" + BLOCKSIZE + ")."); //TODO subclass exception
+		if(IV.length != blockSize || key.length != blockSize)
+			throw new RuntimeException("key and IV need to be same as block size (" + blockSize + ")."); //TODO subclass exception
 		this.key = key;
 		this.IV = IV;
 		cfg = true;
 		blockNo = 0;
-		buffer = new byte[BLOCKSIZE];
+		buffer = new byte[blockSize];
+		bufferIndex = 0;
 	}
 	
 	private byte[] cryptBlock(){
 		byte[] iv = Arrays.copyOf(IV, IV.length);// + BLOCKSIZE);
 		//System.arraycopy(IV, 0, iv, 0, BLOCKSIZE);
-		iv[blockNo % BLOCKSIZE] += blockNo + 1;
+		iv[blockNo % blockSize] += blockNo + 1;
 		//iv = Misc.cleanXOR(iv, key); //for some reason this line runs much faster than the following two lines
 		//Misc.XORintoA(iv, key);
-		iv = Arrays.copyOf(iv, BLOCKSIZE + iv.length);
+		iv = Arrays.copyOf(iv, blockSize + iv.length);
 		//Misc.arraycopy(key, 0, iv, BLOCKSIZE, BLOCKSIZE);
-		System.arraycopy(key, 0, iv, BLOCKSIZE, BLOCKSIZE);
+		System.arraycopy(key, 0, iv, blockSize, blockSize);
 		return Misc.cleanXOR(buffer, mD.digest(iv));
+	}
+	
+	public boolean isConfigured(){
+		return cfg;
 	}
 	
 //	public byte[] updateWithInterrupts(byte[] input){
@@ -131,23 +163,23 @@ public class SHE {
 			input = in2;
 		}
 		
-		int numBlocks = (int) Math.floor(input.length/BLOCKSIZE);
+		int numBlocks = (int) Math.floor(input.length/blockSize);
 		//System.out.println(numBlocks);
-		byte[] out = new byte[BLOCKSIZE * numBlocks];
+		byte[] out = new byte[blockSize * numBlocks];
 		for(int i = 0; i < numBlocks; i++){
 			//System.out.println("i:"+i+" block:" + blockNo);
-			System.arraycopy(input, BLOCKSIZE*i, buffer, 0, BLOCKSIZE);
-			System.arraycopy(cryptBlock(), 0, out, i * BLOCKSIZE, BLOCKSIZE);//TODO do encryption on buffer
+			System.arraycopy(input, blockSize*i, buffer, 0, blockSize);
+			System.arraycopy(cryptBlock(), 0, out, i * blockSize, blockSize);//TODO do encryption on buffer
 			blockNo++;
 		}
-		buffer = new byte[BLOCKSIZE];
-		if(input.length % BLOCKSIZE == 0){
+		buffer = new byte[blockSize];
+		if(input.length % blockSize == 0){
 			
 			bufferIndex = 0;
 		}else{
 			//buffer = new byte[BLOCKSIZE];
-			System.arraycopy(input, numBlocks*BLOCKSIZE, buffer, 0, input.length - numBlocks*BLOCKSIZE);
-			bufferIndex = input.length - numBlocks*BLOCKSIZE;
+			System.arraycopy(input, numBlocks*blockSize, buffer, 0, input.length - numBlocks*blockSize);
+			bufferIndex = input.length - numBlocks*blockSize;
 		}
 		//System.out.println(Misc.bytesToHex(out));
 		return out;
@@ -247,6 +279,73 @@ public class SHE {
 		return out;
 	}
 	
+	public static class EncryptedOutputStream extends FilterOutputStream{
+		private SHE cipher;
+		/**
+		 * If true, when this stream is closed using its close() method, any 
+		 * unprocessed data will be padded and then encrypted.
+		 * If false, when this stream is closed any unprocessed data will be discarded.
+		 */
+		public boolean padOnClose;
+		
+		/**
+		 * Constructs an EncryptedOutputStream from an OutputStream and a Cipher. 
+		 * Note: if the specified output stream or cipher is null, a NullPointerException may be thrown later when they are used.
+		 * @param out the OutputStream object
+		 * @param cipher an initialized Cipher object
+		 */
+		public EncryptedOutputStream(OutputStream out, SHE cipher) {
+			super(out);
+			if(!cipher.isConfigured())
+				throw new RuntimeException("Cipher not configured.");
+			padOnClose = true;
+			this.cipher = cipher;
+		}
+		/**
+		 * Writes the specified byte to this output stream.
+		 */
+		@Override
+		public void write(int b) throws IOException{
+			out.write(cipher.update(new byte[]{(byte) b}));
+		}
+		
+		
+		@Override
+		public void write(byte[] b) throws IOException{
+			//byte[] debug;// = new byte[b.length];
+			//debug = ;
+			//System.out.print(Misc.bytesToHex(debug));
+			out.write(cipher.update(b));
+			//return debug;
+		}
+		
+		public void write(byte[] b, int off, int len) throws IOException{
+			byte[] todo = new byte[len];
+			System.arraycopy(b, off, todo, 0, len);
+			write(todo);
+		}
+		
+		//@Override
+	//	public void flush() throws IOException{
+			//write(cipher.doFinal());
+			//out.flush();
+		//}
+		@Override
+		public void close() throws IOException{
+			if(padOnClose)
+				out.write(cipher.doFinal());
+			out.close();
+		}
+		/**
+		 * 
+		 * @return the underlying SHE cipher for this Stream
+		 */
+		public SHE getCipher(){
+			return cipher;
+		}
+		
+	}
+	
 	public static class EncryptionResult implements Serializable{
 		/**
 		 * 
@@ -305,5 +404,28 @@ public class SHE {
 					+ Misc.bytesToHex(iv) + "[text=" + Misc.bytesToHex(text)+ "]\n" + Misc.bytesToHex(simpleSerial());
 		}
 	}
-
+	public class Algorithm{
+		public static final String SHA1 = "SHA-1";
+		public static final String SHA256 = "SHA-256";
+		public static final String SHA384 = "SHA-384";
+		public static final String SHA512 = "SHA-512";
+		
+		private String algo;
+		public Algorithm(String algorithm){
+			if(!algorithm.equals(SHA1) || !algorithm.equals(SHA256) || 
+					!algorithm.equals(SHA384) || !algorithm.equals(SHA512))
+				throw new RuntimeException("Invalid algorithm " + algorithm);
+			algo = algorithm;
+		}
+		public String getAlgorithm() {
+			return algo;
+		}
+		public void setAlgorithm(String algo) {
+			if(!algo.equals(SHA1) || !algo.equals(SHA256) || 
+					!algo.equals(SHA384) || !algo.equals(SHA512))
+				throw new RuntimeException("Invalid algorithm " + algo);
+			this.algo = algo;
+		}
+		
+	}
 }
