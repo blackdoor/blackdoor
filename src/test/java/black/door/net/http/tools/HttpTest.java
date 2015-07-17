b76a279d4e1bbc298b52cc79d3b98f25dc899715
Cj Buresch <cjburesch@gmail.com>
2015-07-17 14:39:48 -0700

    Fixed test

--- src/test/java/black/door/net/http/tools/HttpTest.java
+++ src/test/java/black/door/net/http/tools/HttpTest.java
@@ -1,10 +1,17 @@
-package black.door.net.http.tools;
+package java.black.door.net.http.tools;
+
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
+
+import black.door.net.http.tools.HttpRequest;
+import black.door.net.http.tools.HttpResponse;
+import black.door.net.http.tools.HttpVerb;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
+import java.util.Map;
 
 /**
  * Created by nfischer on 6/9/15.
@@ -13,19 +20,33 @@ public class HttpTest {
 
     @Test
     public void httpResponseTest() throws IOException {
-        String response = "HTTP/1.0 200 OK\n" +
+        String responsestring = "HTTP/1.0 200 OK\n" +
                 "Date: Fri, 31 Dec 1999 23:59:59 GMT";
-        System.out.println(new HttpResponse(response.getBytes(StandardCharsets.US_ASCII)));
-
-        String responseWBody = "HTTP/1.0 200 OK\n" +
+        
+        HttpResponse response = new HttpResponse(responsestring.getBytes(StandardCharsets.US_ASCII));
+    	Map<String, String> mp = response.getHeaders();
+    	
+		assertEquals( "HTTP/1.0",response.getVersion());
+		assertEquals( 200,response.getStatusCode());
+		assertEquals("Fri, 31 Dec 1999 23:59:59 GMT",mp.get("Date"));
+        
+        String responseWBodystring = "HTTP/1.0 200 OK\n" +
                 "Date: Fri, 31 Dec 1999 23:59:59 GMT\n" +
                 "Content-Type: application/x-www-form-urlencoded\n" +
                 "Content-Length: 32\n" +
                 "\n" +
                 "home=Cosby&favorite+flavor=flies";
-
-        System.out.println(new HttpResponse(responseWBody.getBytes(StandardCharsets.US_ASCII)) + "\n");
-
+        
+        HttpResponse responseWBody = new HttpResponse(responseWBodystring.getBytes(StandardCharsets.US_ASCII));
+        mp = responseWBody.getHeaders();
+        
+		assertEquals( "HTTP/1.0",responseWBody.getVersion());
+		assertEquals( 200,responseWBody.getStatusCode());
+		assertEquals("Fri, 31 Dec 1999 23:59:59 GMT",mp.get("Date"));
+		assertEquals("32",mp.get("Content-Length"));
+		assertEquals("application/x-www-form-urlencoded",mp.get("Content-Type"));
+		assertEquals("home=Cosby&favorite+flavor=flies",new String(responseWBody.getBody()));
+        
         String chunkedResponse = "HTTP/1.0 200 OK\n" +
                 "Date: Fri, 31 Dec 1999 23:59:59 GMT\n" +
                 "Transfer-Encoding: nop\n" +
@@ -38,25 +59,53 @@ public class HttpTest {
                 " in\r\n\r\nchunks.\r\n" +
                 "0\r\n" +
                 "\r\n";
-        System.out.println(new HttpResponse(chunkedResponse.getBytes(StandardCharsets.US_ASCII)));
+        
+		assertEquals( "HTTP/1.0",responseWBody.getVersion());
+		assertEquals( 200,responseWBody.getStatusCode());
+		assertEquals("Fri, 31 Dec 1999 23:59:59 GMT",mp.get("Date"));
+		assertEquals("32",mp.get("Content-Length"));
+		assertEquals("application/x-www-form-urlencoded",mp.get("Content-Type"));
+		//TODO 
     }
 
     @Test
     public void httpRequestTest() throws IOException, URISyntaxException {
-        String get = "GET /path/file.html HTTP/1.0\n" +
-                "From: someuser@jmarshall.com\n" +
+       // GET
+    	String getrequeststring = "GET /path/file.html HTTP/1.0\n" +
+                "From: frog@jmarshall.com\n" +
                 "User-Agent: HTTPTool/1.0\n\n";
-        System.out.println(new HttpRequest(get.getBytes(StandardCharsets.US_ASCII)));
-
-        String post = "POST /path/script.cgi HTTP/1.0\n" +
+		
+        HttpRequest get = new HttpRequest(getrequeststring.getBytes(StandardCharsets.US_ASCII));
+    	Map<String, String> mp = get.getHeaders();
+    	
+        assertEquals(HttpVerb.GET,get.getVerb());
+		assertEquals("/path/file.html",get.getUri().toString());
+		assertEquals( "HTTP/1.0",get.getVersion());
+		assertEquals("frog@jmarshall.com",mp.get("From"));
+		assertEquals("HTTPTool/1.0",mp.get("User-Agent"));
+        
+		//POST
+        String postrequeststring = "POST /path/script.cgi HTTP/1.0\n" +
                 "From: frog@jmarshall.com\n" +
                 "User-Agent: HTTPTool/1.0\n" +
                 "Content-Type: application/x-www-form-urlencoded\n" +
                 "Content-Length: 32\n" +
                 "\n" +
                 "home=Cosby&favorite+flavor=flies";
-        System.out.println(new HttpRequest(post.getBytes(StandardCharsets.US_ASCII)));
+		
+        HttpRequest post = new HttpRequest(postrequeststring.getBytes(StandardCharsets.US_ASCII));
+		mp = post.getHeaders();
+	
+		assertEquals(HttpVerb.POST,post.getVerb());
+		assertEquals("/path/script.cgi",post.getUri().toString());
+		assertEquals( "HTTP/1.0",post.getVersion());
+		assertEquals("frog@jmarshall.com",mp.get("From"));
+		assertEquals("HTTPTool/1.0",mp.get("User-Agent"));
+		assertEquals("application/x-www-form-urlencoded",mp.get("Content-Type"));
+		assertEquals( "32",mp.get("Content-Length"));
+		assertEquals("home=Cosby&favorite+flavor=flies",new String(post.getBody()));
 
+		// Chunked POST
         String chunked = "POST /path/script.cgi HTTP/1.0\n" +
                 "From: frog@jmarshall.com\n" +
                 "User-Agent: HTTPTool/1.0\n" +
@@ -71,7 +120,17 @@ public class HttpTest {
                 " in\r\n\r\nchunks.\r\n" +
                 "0\r\n" +
                 "\r\n";
-        System.out.println(new HttpRequest(chunked.getBytes(StandardCharsets.US_ASCII)));
+       
+        HttpRequest chunkedpost = new HttpRequest(chunked.getBytes(StandardCharsets.US_ASCII));
+		mp = chunkedpost.getHeaders();
+		assertEquals(HttpVerb.POST,chunkedpost.getVerb());
+		assertEquals("/path/script.cgi",chunkedpost.getUri().toString());
+		assertEquals( "HTTP/1.0",chunkedpost.getVersion());
+		assertEquals("frog@jmarshall.com",mp.get("From"));
+		assertEquals("HTTPTool/1.0",mp.get("User-Agent"));
+		assertEquals("nop",mp.get("Transfer-Encoding"));
+		assertEquals("application/x-www-form-urlencoded",mp.get("Content-Type"));
+		//TODO
     }
 
 }
